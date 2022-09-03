59ce64c3efad380bda0b04be3b48bca089caaba2
dreamer-sophia <zzm_mail2022@163.com>
2022-09-03 11:20:11 +0800

    remove

--- springboot-gw-mybatis-plus版/src/main/java/com/dove/demo/mapper/GateWayMapper.java
+++
@@ -1,8 +0,0 @@
-package com.dove.demo.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.dove.demo.entity.GateWay;
-
-//@Mapper
-public interface GateWayMapper extends BaseMapper<GateWay> {
-}
