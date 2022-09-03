53d67b9165a07f175869ab0b520868d2784a674e
dreamer-sophia <zzm_mail2022@163.com>
2022-09-03 11:21:33 +0800

    remove

--- project/springboot-mybatis-plus版/src/main/java/com/dove/demo/mapper/DeviceMapper.java
+++
@@ -1,8 +0,0 @@
-package com.dove.demo.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.dove.demo.entity.Device;
-
-//@Mapper
-public interface DeviceMapper extends BaseMapper<Device> {
-}
