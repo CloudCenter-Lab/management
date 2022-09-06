fa2483c648a4301c7866c8114cab02a1d1ab3180
zy1186740723 <1186740723@qq.com>
2022-09-06 09:41:50 +0800

    add results

--- src/test/java/com/gh/management/system/ServiceTest.java
+++ src/test/java/com/gh/management/system/ServiceTest.java
@@ -1,6 +1,8 @@
 package com.gh.management.system;
 
+import com.gh.management.system.pojo.Device;
 import com.gh.management.system.pojo.User;
+import com.gh.management.system.service.DeviceService;
 import com.gh.management.system.service.UserService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,6 +19,8 @@ public class ServiceTest {
 
     @Autowired
     private UserService userService;
+    @Autowired
+    private DeviceService deviceService;
     @Test
     public void save() {
 //        User user1=new User();
@@ -30,8 +34,18 @@ public class ServiceTest {
 //        System.out.println(b);
 //        boolean b = userService.removeById(14);
 //        System.out.println(b);
-        List<User> list = userService.list();
-        list.forEach(System.out::println);
+        Device device=new Device();
+        device.setCategory("1");
+        device.setAuxiliaryData("hd");
+        device.setId(1);
+        device.setEid("eid1");
+        device.setIp("192.168.1.2");
+        device.setPk("pk");
+        device.setValidTime(System.currentTimeMillis());
+        deviceService.save(device);
+
+//        List<User> list = userService.list();
+//        list.forEach(System.out::println);
 
     }
 
