78575bfdfdacf06bbaf0b7e461878672c47c77f9
dreamer-sophia <zzm_mail2022@163.com>
2022-09-07 11:25:35 +0800

    提交

---
+++ management-master/src/main/java/com/gh/management/system/mapper/DeviceMapper.java
@@ -0,0 +1,26 @@
+package com.gh.management.system.mapper;
+
+import com.gh.management.system.pojo.Device;
+import org.apache.ibatis.annotations.*;
+
+import java.util.List;
+
+@Mapper
+public interface DeviceMapper {
+    @Select("select * from device")
+    List<Device> findAll();
+
+    @Insert("insert into device(id,eid,ip,category,pk,validTime,auxiliaryData) values (#{id},#{eid},#{ip},#{category},#{pk},#{validTime},#{auxiliaryData})")
+    int insert(Device device);
+
+    int update(Device device);
+
+    @Delete("delete from device where id=#{id}")
+    Integer deleteById(@Param("id") String id);
+
+    @Select("select * from device limit #{pageNum},#{pageSize}")
+    List<Device> selectPage(Integer pageNum, Integer pageSize);
+
+    @Select("select count(*) from device ")
+    Integer selectTotal();
+}
---
+++ management-master/src/test/java/com/gh/management/system/ServiceTest.java
@@ -0,0 +1,117 @@
+package com.gh.management.system;
+
+<<<<<<< HEAD
+import com.gh.management.system.pojo.Device;
+import com.gh.management.system.pojo.User;
+import com.gh.management.system.service.DeviceService;
+=======
+<<<<<<< HEAD
+import com.gh.management.system.pojo.Device;
+import com.gh.management.system.pojo.User;
+import com.gh.management.system.service.DeviceService;
+=======
+<<<<<<< HEAD
+import com.gh.management.system.pojo.Device;
+import com.gh.management.system.pojo.User;
+import com.gh.management.system.service.DeviceService;
+=======
+<<<<<<< HEAD
+import com.gh.management.system.pojo.Device;
+import com.gh.management.system.pojo.User;
+import com.gh.management.system.service.DeviceService;
+=======
+import com.gh.management.system.pojo.User;
+>>>>>>> 01cba11 (submit)
+>>>>>>> e243474 (submit)
+>>>>>>> 890ad90 (submit)
+>>>>>>> b98947e (提交)
+import com.gh.management.system.service.UserService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+/**
+ * @author YJL
+ * @create 2022-08-26 15:31
+ */
+@SpringBootTest
+public class ServiceTest {
+
+    @Autowired
+    private UserService userService;
+<<<<<<< HEAD
+    @Autowired
+    private DeviceService deviceService;
+=======
+<<<<<<< HEAD
+    @Autowired
+    private DeviceService deviceService;
+=======
+<<<<<<< HEAD
+    @Autowired
+    private DeviceService deviceService;
+=======
+<<<<<<< HEAD
+    @Autowired
+    private DeviceService deviceService;
+=======
+>>>>>>> 01cba11 (submit)
+>>>>>>> e243474 (submit)
+>>>>>>> 890ad90 (submit)
+>>>>>>> b98947e (提交)
+    @Test
+    public void save() {
+//        User user1=new User();
+//        user1.setUsername("www");
+//        user1.setPassword("qqqwww");
+//        user1.setAddress("qqq");
+//        user1.setPhone("qqq");
+//        user1.setEmail("qqq");
+//        user1.setSex(SexEnum.FEMALE);
+//        boolean b = userService.save(user1);
+//        System.out.println(b);
+//        boolean b = userService.removeById(14);
+//        System.out.println(b);
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+>>>>>>> e243474 (submit)
+>>>>>>> 890ad90 (submit)
+>>>>>>> b98947e (提交)
+        Device device=new Device();
+        device.setCategory("1");
+        device.setAuxiliaryData("hd");
+        device.setId(1);
+        device.setEid("eid1");
+        device.setIp("192.168.1.2");
+        device.setPk("pk");
+        device.setValidTime(System.currentTimeMillis());
+        deviceService.save(device);
+
+//        List<User> list = userService.list();
+//        list.forEach(System.out::println);
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+=======
+=======
+        List<User> list = userService.list();
+        list.forEach(System.out::println);
+>>>>>>> 01cba11 (submit)
+>>>>>>> e243474 (submit)
+>>>>>>> 890ad90 (submit)
+>>>>>>> b98947e (提交)
+
+    }
+
+
+
+}
